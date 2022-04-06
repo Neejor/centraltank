c988782064448e78ab040188334d6e1ec748baa6
Neejor <neejorchakma@gmail.com>
2022-04-06 11:26:03 +0530

    SDSA

--- index.js
+++ index.js
@@ -5,9 +5,42 @@ app.use(cors());
 app.use(express.json());
 
 var path = require("path");
+const papa = require("papaparse")
+const request = require("request");
+const fs = require('fs');
+const MLR = require("ml-regression-multivariate-linear");
+
+const options = {/* options */};
+// console.log(data);
+console.log(options)
+	const dataStream = fs.createReadStream("./myfile.csv");
+	const parseStream = papa.parse(papa.NODE_STREAM_INPUT, options);
+
+	dataStream.pipe(parseStream);
+
+	let data = [];
+	let X = [];
+	let Y = [];
+	parseStream.on("data", chunk => {
+	    data.push(chunk);
+	});
+
+	let mlr;
+	parseStream.on("finish", () => {
+	    // console.log(data);
+	    console.log(data.length);
+	    for(let i = 1; i<data.length; i++) {
+	    	X.push([parseInt(data[i][0]), parseInt(data[i][1])]);
+	    	Y.push([parseInt(data[i][2])]);
+	    }
+	    console.log(X);
+	    mlr = new MLR(X, Y);
+		console.log(mlr.predict([3, 3]));
+	});
+
 
 app.get("/getPredictedVal", (req,res) => {
-	res.send({val: 3, updatedValue: false});
+	res.send({val: "3", updatedValue: false});
 })
 
 app.get('/', function(req, res) {
